d6381426aafe15c401ac5495e2897be64504661f
Aj7Ay <110721907+Aj7Ay@users.noreply.github.com>
2023-09-04 14:02:47 +0530

    Update Jenkins.sh

--- INSTALLATION_SCRIPTS/Jenkins.sh
+++ INSTALLATION_SCRIPTS/Jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt upgrade -y
+#sudo apt upgrade -y
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
 echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
 sudo apt update -y
