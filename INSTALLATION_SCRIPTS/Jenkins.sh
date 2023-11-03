477d73e75ec9a48856eac6f5dca263cf0c3a70ea
Aj7Ay <110721907+Aj7Ay@users.noreply.github.com>
2023-11-03 21:08:36 +0530

    Update Jenkins.sh

--- INSTALLATION_SCRIPTS/Jenkins.sh
+++ INSTALLATION_SCRIPTS/Jenkins.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt update -y
-#sudo apt upgrade -y
-wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
-echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+sudo touch /etc/apt/keyrings/adoptium.asc
+sudo wget -O /etc/apt/keyrings/adoptium.asc https://packages.adoptium.net/artifactory/api/gpg/key/public
+echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
 sudo apt update -y
 sudo apt install temurin-17-jdk -y
 /usr/bin/java --version
